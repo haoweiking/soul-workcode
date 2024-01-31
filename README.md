219f4f2eb60095de2ac62cb9218bb443846afc2d
sansuiban <wanghao@beiye.ai>
2024-01-31 17:51:03 +0800

    add pdf_add_qr script

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # soul-workcode
-脚本 集锦
 
-rename.py   is a function for rename files in folder. 
-
-read_files_to_excel.py  is a script for list files name and number to excel by the dir name
-gen_files_name_to_excel.py  same to read_files_to_excel
+|Script|Description|
+|---|---|
+|*rename.py* |  is a function for rename files in folder. |
+|*read_files_to_excel.py* | is a script for list files name and number to excel by the dir name |
+|*gen_files_name_to_excel.py* | same to read_files_to_excel|
+|*pdf_add_qr.py* | add qr image to pdf|
---
+++ pdf_add_qr.py
@@ -0,0 +1,44 @@
+from PyPDF2 import PdfReader, PdfWriter
+import os
+
+
+def add_qr_code_to_pdf(image_pdf_path, pdf_path, write_path):
+    pdf_reader = PdfReader(pdf_path)
+
+    '''
+    first page to writer
+    QR code to writer
+    reader remaining pages to writer
+    # writer insert_page 为倒序
+    '''
+
+    write_path = "var/test_cover.pdf"
+    pdf_write = PdfWriter()
+
+    # reader remaining pages to writer
+    for index in range(len(pdf_reader.pages) - 1, 0, -1):
+        print(index)
+        pdf_write.insert_page(pdf_reader.pages[index])
+
+    # QR code to writer
+    qr_reader = PdfReader(image_pdf_path)
+    pdf_write.insert_page(qr_reader.pages[0])
+
+    # first page to writer
+    pdf_write.insert_page(pdf_reader.pages[0])
+
+    with open(write_path, "wb") as output_stream:
+        pdf_write.write(output_stream)
+
+
+if __name__ == "__main__":
+    current_path = os.getcwd()
+    qr_pdf_path = f"{current_path}/cover.pdf"
+
+    for pdf_file in os.listdir("."):
+        if pdf_file.endswith(".pdf"):
+            pdf_path = os.path.join(".", pdf_file)
+            output_path = f"output_{pdf_file}"
+            add_qr_code_to_pdf(qr_pdf_path, pdf_path, output_path)
+            print(f"Image added to {pdf_file} and saved as {output_path}")
+    
